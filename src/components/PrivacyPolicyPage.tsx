33d20419c7a5feca2f7bda355b65e3f00aaa15f2
NikolaTeslaCoil <kontakt@brandboostmedia.no>
2026-02-26 21:44:11 +0100

    Updated PrivacyPolicyPage.tsx

--- src/components/PrivacyPolicyPage.tsx
+++ src/components/PrivacyPolicyPage.tsx
@@ -114,7 +114,7 @@ export function PrivacyPolicyPage({ darkMode }: PrivacyPolicyPageProps) {
                   <li>Sletting av dine personopplysninger</li>
                   <li>Begrensning av behandling</li>
                   <li>Dataportabilitet</li>
-                  <li>Å protestere mot behandling Design template by [Abdellah Rayen] (CC BY 4.0)</li>
+                  <li>Å protestere mot behandling</li>
                 </ul>
               </section>
 
